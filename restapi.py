d99a644f8f6bbb497c9fe99c77df2b0972cf8d7c
mathmaa <mathmaa@gmail.com>
2024-12-16 21:45:05 +0100

    Comments and fixing posting w/o description

--- restapi.py
+++ restapi.py
@@ -8,6 +8,7 @@ class Task:
         self.title = title
         self.description = description
     
+    # Convert the object to a JSON representation.
     def to_json(self):
         return {
             "id": self.id,
@@ -19,9 +20,7 @@ app = Flask(__name__)
 
 tasks = []
 
-#tasks.append(Task(0,"First", "This is the first task"))
-#tasks.append(Task(1,"Second", "This is the second task"))
-
+# When the API initializes, read the saved tasks into memory.
 with app.app_context():
     with open('tasklist.json') as json_file:
         data = json.load(json_file)
@@ -31,53 +30,65 @@ with app.app_context():
 
 @app.route('/task', methods=['GET', 'POST'])
 def get_all_or_post_task():
-
+    
+    # Return all the tasks in the list as JSON representations.
     if request.method == 'GET':
         return [task.to_json() for task in tasks]
     
+    # Add an item to the list.
     elif request.method == 'POST':
         data = request.form
 
-        if 'id' not in data:
-            id = new_unique_id()
+        # The following lines of code checks if an id is valid.
+        # If no id is given, the program will generate a new unique id for the entry.
+        if 'id' not in data:            
+            id = new_unique_id()  
         else:
-            try: 
+            try:                       
                 id = int(data['id'])
             except ValueError:
                 abort(400, description='Id is not an integer')
             
-            if not is_unique(int(id)):
+            if not is_unique(int(id)):  
                 abort(400, description='Id is not unique')
             else:
-                id = int(data['id'])
-
-        if 'title' not in data:
-            abort(400, description='Missing title')
+                id = int(data['id'])    
+        
+        # The following code checks if the POST request includes a title and then if the title is empty. 
+        if 'title' not in data:         
+            abort(400, description='Missing title') 
         else:
             title = data['title']
             if title == "":
                 abort(400, description='Title is empty')
-
+        
+        # The following code checks if the POST request includes a description.
         if 'description' in data:
             description = data['description']
+        else:
+            description = ""
 
         tasks.append(Task(id,title,description))
 
         return "Added to list with id: " + str(tasks[len(tasks) - 1].id)
 
+
 @app.route('/task/<int:id>', methods=['GET', 'DELETE', 'PUT'])
 def get_delete_update_task(id):
+    # Loop over the tasks until the one with the given id is selected.
     for task in tasks:
         if task.id == id:
             
+            # Return the task as JSON representation.
             if request.method == 'GET':
                 return [task.to_json()]
             
+            # Remove the task.
             elif request.method == 'DELETE':
-                task_to_delete = task
                 tasks.remove(task)
                 return "Task deleted"
             
+            # Update the values submitted.
             elif request.method == 'PUT':
                 data = request.form
                 if 'title' in data:
@@ -88,6 +99,7 @@ def get_delete_update_task(id):
             
     return "Task not found"
 
+# Create a new, unique id that is the lowest available integer not used by another item.
 def new_unique_id():
     list_of_ids = [task.id for task in tasks]
     list_of_ids.sort()
@@ -99,6 +111,7 @@ def new_unique_id():
             last_id = id
     return last_id + 1
 
+# Check if an id is unique.
 def is_unique(id):
     for task in tasks:
         if task.id == id:
